1669f5ea827eb67c0defb7c31e7d3fea0907b043
Dhawalgit09 <dhawalsrivastava2583@gmail.com>
2025-09-01 00:06:11 +0530

    fix

--- Smart-recipe-Generator/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Atlas Connected"))
-  .catch(err => console.error("❌ DB Connection Error:", err));
